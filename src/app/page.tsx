d7fbcb4a3079adeaa3f2ac4e6ba17228b5b4e0d6
Himanshu4252 <himanshuhddudhpuri4252@gmail.com>
2024-08-22 20:55:20 +0530

    passed production build check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import LogSignBtn from "./loginBtn/page";
-import Example from "./Example";
 
 const Homepage = () => {
   return (<>
     <div>Homepage</div>
-    <div style={{position:"absolute", zIndex:5, width:"95vw", display:"flex", flexDirection:"row", justifyContent:"flex-end"}}>
+    <div style={{position:"absolute", zIndex:5, width:"95vw", display:"flex", flexDirection:"row", justifyContent:"flex-end" }}>
     <LogSignBtn />
     </div>
     </>

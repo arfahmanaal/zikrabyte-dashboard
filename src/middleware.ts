b3a9e9a408ea4a93f3606fc44b5ec1fc30ca86c0
Himanshu4252 <himanshuhddudhpuri4252@gmail.com>
2024-08-25 00:27:36 +0530

    updated Paths for homepage and Dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,11 +10,9 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
   }
   if(path === '/' && token){
-    console.log("not signed in and you are at homepage so no redirection");
     return NextResponse.redirect(new URL('/dashboard', request.nextUrl));
   }
   if(path === '/' && !token){
-    console.log("not signed in and want to go back to hompage so go ..");
     return NextResponse.next();
   }
   if (!isPublicPath && !token) {
